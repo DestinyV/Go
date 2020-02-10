fc262693095be5341c22424f22dab33f45b15ed7
DestinyV <37183101+DestinyV@users.noreply.github.com>
2020-02-10 21:27:14 +0800

    :beer: 添加channel文件

---
+++ src/golang.learn.com/golang-learn/channel-learn/main.go
@@ -0,0 +1,152 @@
+package main
+
+import "fmt"
+
+// channel
+// 单纯地将函数并发执行是没有意义的。函数与函数间需要交换数据才能体现并发执行函数的意义。
+
+// 虽然可以使用共享内存进行数据交换，但是共享内存在不同的goroutine中容易发生竞态问题。为了保证数据交换的正确性，必须使用互斥量对内存进行加锁，这种做法势必造成性能问题。
+
+// Go语言的并发模型是CSP（Communicating Sequential Processes），提倡通过通信共享内存而不是通过共享内存而实现通信。
+
+// 如果说goroutine是Go程序并发的执行体，channel就是它们之间的连接。channel是可以让一个goroutine发送特定值到另一个goroutine的通信机制。
+
+// Go 语言中的通道（channel）是一种特殊的类型。通道像一个传送带或者队列，总是遵循先入先出（First In First Out）的规则，保证收发数据的顺序。每一个通道都是一个具体类型的导管，也就是声明channel的时候需要为其指定元素类型。
+func main() {
+	// 	channel类型
+	// channel是一种类型，一种引用类型。声明通道类型的格式如下：
+
+	// var 变量 chan 元素类型
+	// 举几个例子：
+
+	// var ch1 chan int   // 声明一个传递整型的通道
+	// var ch2 chan bool  // 声明一个传递布尔型的通道
+	// var ch3 chan []int // 声明一个传递int切片的通道
+	// var ch8 chan string
+	// 	创建channel
+	// 通道是引用类型，通道类型的空值是nil。
+
+	// var ch chan int
+	// fmt.Println(ch) // <nil>
+
+	// 声明的通道后需要使用make函数初始化之后才能使用。
+
+	// 创建channel的格式如下：
+
+	// make(chan 元素类型, [缓冲大小])
+	// channel的缓冲大小是可选的。
+
+	// 举几个例子：
+
+	// ch4 := make(chan int)
+	// ch5 := make(chan bool)
+	// ch6 := make(chan []int)
+	// ch9 := make(chan string)
+
+	// 	channel操作
+	// 通道有发送（send）、接收(receive）和关闭（close）三种操作。
+
+	// 发送和接收都使用<-符号。
+
+	// 现在我们先使用以下语句定义一个通道：
+
+	// ch7 := make(chan int)
+	// 发送
+	// 将一个值发送到通道中。
+
+	// ch7 <- 10 // 把10发送到ch中
+	// 接收
+	// 从一个通道中接收值。
+
+	// x := <-ch7 // 从ch中接收值并赋值给变量x
+	// <-ch7      // 从ch中接收值，忽略结果
+	// 关闭
+	// 我们通过调用内置的close函数来关闭通道。
+
+	// close(ch7)
+	// fmt.Println(x)
+	// 关于关闭通道需要注意的事情是，只有在通知接收方goroutine所有的数据都发送完毕的时候才需要关闭通道。通道是可以被垃圾回收机制回收的，它和关闭文件是不一样的，在结束操作之后关闭文件是必须要做的，但关闭通道不是必须的。
+
+	// 关闭后的通道有以下特点：
+
+	// 对一个关闭的通道再发送值就会导致panic。
+	// 对一个关闭的通道进行接收会一直获取值直到通道为空。
+	// 对一个关闭的并且没有值的通道执行接收操作会得到对应类型的零值。
+	// 关闭一个已经关闭的通道会导致panic。
+
+	ch := make(chan int) // ch := make(chan int)创建的是无缓冲的通道，无缓冲的通道只有在有人接收值的时候才能发送值。
+	// 	无缓冲通道上的发送操作会阻塞，直到另一个goroutine在该通道上执行接收操作，这时值才能发送成功，两个goroutine将继续执行。相反，如果接收操作先执行，接收方的goroutine将阻塞，直到另一个goroutine在该通道上发送一个值。
+
+	// 使用无缓冲通道进行通信将导致发送和接收的goroutine同步化。因此，无缓冲通道也被称为同步通道。
+	go receiveChan(ch)
+	ch <- 10
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	// 开启goroutine将0~100的数发送到ch1中
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+	// 开启goroutine从ch1中接收值，并将该值的平方发送到ch2中
+	go func() {
+		for {
+			i, ok := <-ch1 // 通道关闭后再取值ok=false
+			if !ok {
+				break
+			}
+			ch2 <- i * i
+		}
+		close(ch2)
+	}()
+	// 在主goroutine中从ch2中接收值打印
+	for i := range ch2 { // 通道关闭后会退出for range循环
+		fmt.Println("number form ch2:", i)
+	}
+}
+
+func receiveChan(rec chan int) {
+	num := <-rec
+	close(rec)
+	fmt.Println("receiveChan number:", num)
+}
+
+// 单向通道
+// 有的时候我们会将通道作为参数在多个任务函数间传递，很多时候我们在不同的任务函数中使用通道都会对其进行限制，比如限制通道在函数中只能发送或只能接收。
+
+// Go语言中提供了单向通道来处理这种情况。例如，我们把上面的例子改造如下：
+
+func counter(out chan<- int) {
+	for i := 0; i < 100; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for i := range in {
+		out <- i * i
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for i := range in {
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go counter(ch1)
+	go squarer(ch2, ch1)
+	printer(ch2)
+}
+
+// 其中，
+
+// chan<- int是一个只写单向通道（只能对其写入int类型值），可以对其执行发送操作但是不能执行接收操作；
+// <-chan int是一个只读单向通道（只能从其读取int类型值），可以对其执行接收操作但是不能执行发送操作。
+// 在函数传参及任何赋值操作中可以将双向通道转换为单向通道，但反过来是不可以的。
--- src/golang.learn.com/golang-learn/concurrency-learn/main.go
+++ src/golang.learn.com/golang-learn/concurrency-learn/main.go
@@ -1,11 +1,96 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
 // 并发：同一时间段内执行多个任务。
 // 并行：同一时刻执行多个任务。
 
 // Go语言的并发通过goroutine实现。goroutine类似于线程，属于用户态的线程，我们可以根据需要创建成千上万个goroutine并发工作。goroutine是由Go语言的运行时（runtime）调度完成，而线程是由操作系统调度完成。
 
 // Go语言还提供channel在多个goroutine间进行通信。goroutine和channel是 Go 语言秉承的 CSP（Communicating Sequential Process）并发模式的重要实现基础。
-func main() {
 
+// goroutine
+
+// Go语言中的goroutine就是这样一种机制，goroutine的概念类似于线程，但 goroutine是由Go的运行时（runtime）调度和管理的。Go程序会智能地将 goroutine 中的任务合理地分配给每个CPU。Go语言之所以被称为现代化的编程语言，就是因为它在语言层面已经内置了调度和上下文切换的机制。
+
+// 在Go语言编程中你不需要去自己写进程、线程、协程，你的技能包里只有一个技能–goroutine，当你需要让某个任务并发执行的时候，你只需要把这个任务包装成一个函数，开启一个goroutine去执行这个函数就可以了，就是这么简单粗暴。
+
+// 使用goroutine
+// Go语言中使用goroutine非常简单，只需要在调用函数的时候在前面加上go关键字，就可以为一个函数创建一个goroutine。
+
+// 一个goroutine必定对应一个函数，可以创建多个goroutine去执行相同的函数。
+
+// 在程序启动时，Go程序就会为main()函数创建一个默认的goroutine。
+
+// 当main()函数返回的时候该goroutine就结束了，所有在main()函数中启动的goroutine会一同结束，
+var wg sync.WaitGroup
+
+func main() {
+	go printRoutine()
+	fmt.Println("main goroutine done!")
+	time.Sleep(time.Second * 3)
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // 启动一个goroutine就登记+1
+		go hello(i)
+	}
+	wg.Wait()             // 等待所有登记的goroutine都结束
+	runtime.GOMAXPROCS(2) // 默认跑所有逻辑核心数
+	go a()
+	go b()
+	time.Sleep(time.Second)
 }
+
+func hello(i int) {
+	defer wg.Done() // goroutine结束就登记-1
+	fmt.Println("Hello Goroutine!", i)
+}
+
+func printRoutine() {
+	fmt.Println("printRoutine")
+}
+
+// goroutine与线程
+// 可增长的栈
+// OS线程（操作系统线程）一般都有固定的栈内存（通常为2MB）,一个goroutine的栈在其生命周期开始时只有很小的栈（典型情况下2KB），goroutine的栈不是固定的，他可以按需增大和缩小，goroutine的栈大小限制可以达到1GB，虽然极少会用到这个大。所以在Go语言中一次创建十万左右的goroutine也是可以的。
+
+// goroutine调度
+// GPM是Go语言运行时（runtime）层面的实现，是go语言自己实现的一套调度系统。区别于操作系统调度OS线程。
+
+// G很好理解，就是个goroutine的，里面除了存放本goroutine信息外 还有与所在P的绑定等信息。
+// P管理着一组goroutine队列，P里面会存储当前goroutine运行的上下文环境（函数指针，堆栈地址及地址边界），P会对自己管理的goroutine队列做一些调度（比如把占用CPU时间较长的goroutine暂停、运行后续的goroutine等等）当自己的队列消费完了就去全局队列里取，如果全局队列里也消费完了会去其他P的队列里抢任务。
+// M（machine）是Go运行时（runtime）对操作系统内核线程的虚拟， M与内核线程一般是一一映射的关系， 一个groutine最终是要放到M上执行的；
+// P与M一般也是一一对应的。他们关系是： P管理着一组G挂载在M上运行。当一个G长久阻塞在一个M上时，runtime会新建一个M，阻塞G所在的P会把其他的G 挂载在新建的M上。当旧的G阻塞完成或者认为其已经死掉时 回收旧的M。
+
+// P的个数是通过runtime.GOMAXPROCS设定（最大256），Go1.5版本之后默认为物理线程数。 在并发量大的时候会增加一些P和M，但不会太多，切换太频繁的话得不偿失。
+
+// 单从线程调度讲，Go语言相比起其他语言的优势在于OS线程是由OS内核来调度的，goroutine则是由Go运行时（runtime）自己的调度器调度的，这个调度器使用一个称为m:n调度的技术（复用/调度m个goroutine到n个OS线程）。 其一大特点是goroutine的调度是在用户态下完成的， 不涉及内核态与用户态之间的频繁切换，包括内存的分配与释放，都是在用户态维护着一块大的内存池， 不直接调用系统的malloc函数（除非内存池需要改变），成本比调度OS线程低很多。 另一方面充分利用了多核的硬件资源，近似的把若干goroutine均分在物理线程上， 再加上本身goroutine的超轻量，以上种种保证了go调度方面的性能。
+
+// GOMAXPROCS
+// Go运行时的调度器使用GOMAXPROCS参数来确定需要使用多少个OS线程来同时执行Go代码。默认值是机器上的CPU核心数。例如在一个8核心的机器上，调度器会把Go代码同时调度到8个OS线程上（GOMAXPROCS是m:n调度中的n）。
+
+// Go语言中可以通过runtime.GOMAXPROCS()函数设置当前程序并发时占用的CPU逻辑核心数。
+
+// Go1.5版本之前，默认使用的是单核心执行。Go1.5版本之后，默认使用全部的CPU逻辑核心数。
+
+func a() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("A:", i)
+	}
+}
+
+func b() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("B:", i)
+	}
+}
+
+// Go语言中的操作系统线程和goroutine的关系：
+
+// 一个操作系统线程对应用户态多个goroutine。
+// go程序可以同时使用多个操作系统线程。
+// goroutine和OS线程是多对多的关系，即m:n。
