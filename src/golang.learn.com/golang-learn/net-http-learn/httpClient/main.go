3d6ea0f834ac60199fb50a6cbec541b1d05af6ed
DestinyV <37183101+DestinyV@users.noreply.github.com>
2020-02-14 20:55:45 +0800

    :mouse: http-client添加自定义client

--- src/golang.learn.com/golang-learn/net-http-learn/httpClient/main.go
+++ src/golang.learn.com/golang-learn/net-http-learn/httpClient/main.go
@@ -7,6 +7,15 @@ import (
 	"net/url"
 )
 
+// 公用一个client适用于连接比较频繁
+var (
+	client = &http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives: true, // 禁用keepalive
+		},
+	}
+)
+
 func main() {
 	// resp, err := http.Get("http://127.0.0.1:8085/v1/f2/?name=lili&age=5")
 
@@ -33,11 +42,19 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	resp, err := http.DefaultClient.Do(req)
+	// resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// 适用于连接不是特别频繁用完就关闭连接
+	client := &http.Client{
+		Transport: &http.Transport{
+			DisableKeepAlives: true, // 禁用keepalive
+		},
+	}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
 	bin, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("err")
