49d2f8a30e1400e0deda04b858f88c513ec47023
DestinyV <37183101+DestinyV@users.noreply.github.com>
2020-02-15 21:23:53 +0800

    :clap: dump-sql、建立user表并查询

--- src/golang.learn.com/golang-learn/MySql-learn/connect-mysql-learn/main.go
+++ src/golang.learn.com/golang-learn/MySql-learn/connect-mysql-learn/main.go
@@ -9,17 +9,48 @@ import (
 
 // _ "github.com/go-sql-driver/mysql"
 
-func main() {
+var db *sql.DB // 是一个连接池对象
+
+func initDB() (err error) {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/golangdb"
-	db, err := sql.Open("mysql", dsn) // open 不会校验用户名和密码是否正确
+	db, err = sql.Open("mysql", dsn) // open 不会校验用户名和密码是否正确
 	if err != nil {
-		fmt.Printf("dsn: %s invalid, err: %v\n", dsn, err)
 		return
 	}
 	err = db.Ping() // 尝试连接数据库
 	if err != nil {
-		fmt.Printf("open: %s failed, err: %v\n", dsn, err)
+		return
+	}
+	// 设置数据库连接池最大连接数
+	db.SetMaxOpenConns(10)
+	return
+}
+
+func main() {
+	err := initDB()
+	if err != nil {
+		fmt.Printf("init db failed, err: %v\n", err)
 		return
 	}
 	fmt.Println("连接数据库成功s")
+	sqlstr := `select id, name, gender, profession from users where id=1;`
+	rowObj := db.QueryRow(sqlstr) // 从连接池中取出一个连接去数据库查询单条记录
+	var u1 user
+	rowObj.Scan(&u1.id, &u1.name, &u1.gender, &u1.profession) //函数都是值拷贝
+	fmt.Printf("query user is :%#v", u1)
+}
+
+type user struct {
+	id         int
+	name       string
+	gender     int
+	profession string
+}
+
+func query() {
+
+}
+
+func insert() {
+
 }
